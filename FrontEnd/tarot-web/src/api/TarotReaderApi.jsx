c1e768b14d033a6df1453d4d1f75d49ec896130a
NguyenPhucPPP <nguyenphucdt36@gmail.com>
2024-08-29 14:47:26 +0700

    [Fix]_Filter tarot reader list_FE

--- FrontEnd/tarot-web/src/api/TarotReaderApi.jsx
+++ FrontEnd/tarot-web/src/api/TarotReaderApi.jsx
@@ -1,12 +1,34 @@
 const baseUrl = import.meta.env.VITE_API_HOST;
 
-export const GetTarotReaderList = async (searchValue,pageNumber, rowsPerpage) => {
+export const GetTarotReaderList = async (searchValue, pageNumber, rowsPerpage, genderFilter, languageFilter, formFilter) => {
     try {
         var url = "";
-        if(searchValue !== '') {
+        if (searchValue !== '') {
             url = `${baseUrl}/api/User/readers?readerName=${encodeURIComponent(searchValue)}&pageNumber=${pageNumber}&rowsPerpage=${rowsPerpage}`;
+            if (languageFilter.length > 0) {
+                const languageFilterParams = languageFilter.map(lang => `filterLanguages=${encodeURIComponent(lang)}`).join('&');
+                url += `&${languageFilterParams}`;
+            }
+            if (genderFilter) {
+                url += `&gender=${encodeURIComponent(genderFilter)}`;
+            }
+            if (formFilter.length > 0) {
+                const formFilterParams = formFilter.map(form => `filterServiceTypes=${encodeURIComponent(form)}`).join('&');
+                url += `&${formFilterParams}`;
+            }
         } else {
             url = `${baseUrl}/api/User/readers?pageNumber=${pageNumber}&rowsPerpage=${rowsPerpage}`;
+            if (languageFilter.length > 0) {
+                const languageFilterParams = languageFilter.map(lang => `filterLanguages=${encodeURIComponent(lang)}`).join('&');
+                url += `&${languageFilterParams}`;
+            }
+            if (genderFilter) {
+                url += `&gender=${encodeURIComponent(genderFilter)}`;
+            }
+            if (formFilter.length > 0) {
+                const formFilterParams = formFilter.map(form => `filterServiceTypes=${encodeURIComponent(form)}`).join('&');
+                url += `&${formFilterParams}`;
+            }
         }
         const request = {
             method: "GET",
@@ -23,9 +45,9 @@ export const GetTarotReaderList = async (searchValue,pageNumber, rowsPerpage) =>
 
 export const GetTarotReaderDetail = async (userId) => {
     try {
-        
+
         const url = `${baseUrl}/api/User/reader-detail?userId=${userId}`;
-        
+
         const request = {
             method: "GET",
             headers: {
