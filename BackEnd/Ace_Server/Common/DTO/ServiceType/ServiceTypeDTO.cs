f1d50ea1b3f3376e0e37f606f46065c7642681b6
anhthien8933@gmail.com <anhthien8933@gmail.com>
2024-08-30 11:40:18 +0700

    Fix TarotReaderDetail

--- BackEnd/Ace_Server/Common/DTO/ServiceType/ServiceTypeDTO.cs
+++ BackEnd/Ace_Server/Common/DTO/ServiceType/ServiceTypeDTO.cs
@@ -8,12 +8,10 @@ namespace Common.DTO.ServiceType
 {
 	public class ServiceTypeDTO
 	{
-		public Guid UserServiceTypeId { get; set; }
-
-		public bool Status { get; set; }
-
-		public Guid UserId { get; set; }
-
 		public Guid ServiceTypeId { get; set; }
+
+		public string ServiceTypeName { get; set; } = null!;
+
+		public bool? Status { get; set; }
 	}
 }
