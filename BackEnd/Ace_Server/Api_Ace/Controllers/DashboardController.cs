30dd1d7c1c35ec6b078cd31a3f44254a58c0080a
anhthien8933@gmail.com <anhthien8933@gmail.com>
2024-10-14 16:04:34 +0700

    Commit

---
+++ BackEnd/Ace_Server/Api_Ace/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using BLL.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Ace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ICardTypeService _cardTypeService;
+        public DashboardController(ICardTypeService cardTypeService)
+        {
+            _cardTypeService = cardTypeService;
+        }
+    }
+}
