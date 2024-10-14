01e5f98dda24fd8e9340f4e94c1ecae3f4021ec3
anhthien8933@gmail.com <anhthien8933@gmail.com>
2024-10-14 22:27:22 +0700

    GetRevenueByTimeRange

--- BackEnd/Ace_Server/Api_Ace/Controllers/DashboardController.cs
+++ BackEnd/Ace_Server/Api_Ace/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using BLL.Interface;
+﻿using System.ComponentModel.DataAnnotations;
+using BLL.Interface;
+using BLL.Services;
+using Common.DTO.General;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +11,31 @@ namespace Api_Ace.Controllers
     [ApiController]
     public class DashboardController : ControllerBase
     {
-        private readonly ICardTypeService _cardTypeService;
-        public DashboardController(ICardTypeService cardTypeService)
+        private readonly IDashboardService _dashboardService;
+        public DashboardController(IDashboardService dashboardService)
         {
-            _cardTypeService = cardTypeService;
+            _dashboardService = dashboardService;
+        }
+        [HttpGet("revenue")]
+        public async Task<IActionResult> GetRevenueByTimeRange([Required] DateOnly startdate,
+                                                                [Required] DateOnly enddate,
+                                                                 [Required] Guid roleid,
+                                                                 Guid tarotReaderId)
+        {
+            ResponseDTO responseDTO = await _dashboardService.GetRevenueByTimeRange(startdate,enddate, roleid, tarotReaderId);
+            if (responseDTO.IsSuccess == false)
+            {
+                if (responseDTO.StatusCode == 404)
+                {
+                    return NotFound(responseDTO);
+                }
+                else
+                {
+                    return BadRequest(responseDTO);
+                }
+            }
+
+            return Ok(responseDTO);
         }
     }
 }
---
+++ BackEnd/Ace_Server/BLL/Services/DashboardService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.Interface;
+using Common.Constant;
+using Common.DTO.General;
+using DAL.Entities;
+using DAL.UnitOfWork;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace BLL.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IUserService _userService;
+        public DashboardService(IUnitOfWork unitOfWork, IMapper mapper, IUserService userService, IBookingService bookingService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _userService = userService;
+        }
+
+        public async Task<ResponseDTO> GetRevenueByTimeRange(DateOnly startdate, DateOnly enddate, Guid roleid, Guid tarotReaderId)
+        {
+            var readerRole =  await _userService.GetReaderRole();
+            if (roleid.Equals(readerRole.RoleId))
+            {
+                if (tarotReaderId == Guid.Empty)
+                {
+                    return new ResponseDTO("Vui lòng nhập Tarot reader Id", 400, false);
+                }
+                else
+                {
+                    var tarotReader = await _userService.GetUserDetailById(tarotReaderId);    
+                    if(tarotReader == null)
+                    {
+                        return new ResponseDTO("Không tồn tại Tarot Reader này", 404, false);
+                    }    
+                }
+            }
+            var list = _unitOfWork.Booking.GetAllByCondition( b=>
+             b.StartTime.Date >= startdate.ToDateTime(TimeOnly.MinValue)
+            && (b.StartTime.Date <= enddate.ToDateTime(TimeOnly.MinValue)));
+            list = list.Where(b => b.Status == BookingStatus.Refunded || b.Status == BookingStatus.ComplaintSuccessfully || b.Status == BookingStatus.Completed);
+            decimal sum = 0;
+            if(tarotReaderId != Guid.Empty)
+            {
+                list = list.Where(b=>b.TarotReaderId.Equals(tarotReaderId));
+            }
+            foreach (var item in list)
+            {
+                if (item.Status == BookingStatus.Refunded || item.Status == BookingStatus.ComplaintSuccessfully)
+                {
+                    item.Price = (decimal)(item.Price - (item.Price * item.ComplaintRefundPercentage / 100));
+                }
+                sum += item.Price;
+            }
+            return new ResponseDTO("Lấy tổng doanh thu theo thời gian thành công", 200, true,sum);
+        }
+    }
+}
